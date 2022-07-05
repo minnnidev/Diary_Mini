1f6c5479144f0c8dd64bd9056b8f5cc2eac7a736
Mini <minnnidev@gmail.com>
2022-07-05 18:22:28 +0900

    Initial Commit

---
+++ Diary_Mini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary_Mini
+//
+//  Created by 김민 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
